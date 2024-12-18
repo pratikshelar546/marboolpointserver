bbabeb1bbcdb9703518dcf5881ff5d1f1a4314b1
pratikshelar546 <66733074+pratikshelar546@users.noreply.github.com>
2024-12-19 00:13:33 +0530

    rmeoved dist

--- dist/config/db.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.client = void 0;
-const pg_1 = require("pg");
-exports.client = new pg_1.Client("postgres://postgres:pratik@localhost:5432/marboolpoint");
--- dist/routes/supplier.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const supplier_1 = require("../controller/supplier");
-const rounter = (0, express_1.Router)();
-rounter.post("/addsupplier", supplier_1.addSupplier);
-exports.default = rounter;
